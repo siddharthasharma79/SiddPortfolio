5b5c794da31175c26647eb704a153e93e22b833c
Siddhartha Sharma <siddharthasharma79@gmail.com>
2019-06-01 18:41:34 +0530

    final commit

--- js/main.js
+++ js/main.js
@@ -6,6 +6,10 @@ function saveBookmark(e) {
     // Get form values
     var siteName = document.getElementById("siteName").value;
     var siteUrl = document.getElementById("siteUrl").value;
+    
+    //Validate Form
+    if(!validateForm(siteName, siteUrl))
+        return false
 
     //Create Object to be saved in local storage
     var bookmark = {
@@ -33,9 +37,36 @@ function saveBookmark(e) {
         // Reset back to local storage
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     }
+
+    // clear form
+    document.getElementById('myForm').reset();
+
     // Prevent form submit 
     e.preventDefault();
+    fetchBookmarks();
     
+}
+
+function deleteBookMark(url) {
+    
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    console.log(bookmarks);
+    for(var i=0; i< bookmarks.length; i++ )
+    {
+        if(bookmarks[i].url == url){
+            console.log(bookmarks[i].url);
+            bookmarks.splice(i, 1);
+            break;
+        }
+    }    
+    // Reset bookmarks in locak storage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+    // Reload bookmarks
+    fetchBookmarks();
+
+    
+
 }
 
 function fetchBookmarks() {
@@ -51,9 +82,30 @@ function fetchBookmarks() {
         var name = bookmarks[i].name;
         var url = bookmarks[i].url;
         bookmarksResults.innerHTML += '<div class="well">'+
-                                           '<h3>'+name+'</h3>'+
+                                           '<h3><a hrer="'+url+'">'+name+'</a>'+
+                                           '<div class="pull-right"> <a class="btn btn-success" target="_blank" href="'+url+'">Visit</a> '+
+                                           ' <a class="btn btn-danger" href="#" onclick="deleteBookMark(\''+url+'\')">Delete</a> </div>'+
+                                           '</h3>'+
                                            '</div>';
     }
+}
+
+function validateForm(siteName, siteUrl) {
+     //validation
+     if(!siteName || !siteUrl){
+        alert('Site Name or Site URL can not be blank.');
+        return false;
+    }   
+    
+    // url validation
+    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+var regex = new RegExp(expression);
+    
+    if(!siteUrl.match(regex)) {
+        alert('Please enter valid site url');
+        return false;
+    }
+    return true;
 }
 
 
