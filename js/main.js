547743f5d35ee5c6b268d75d3bb4aaf2a21c0001
Siddhartha Sharma <siddharthasharma79@gmail.com>
2019-07-11 15:07:45 +0530

    Add sw_site.js file to build entire site cache

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 if('serviceWorker' in navigator) {
      window.addEventListener('load', () => {
         navigator.serviceWorker
-        .register('../sw_pages.js')
+        .register('../sw_site.js')
         .then(reg => console.log("Service Worker: Registerd"))
         .catch(err => console.log(`Service Worker: Error: ${err}`));
      });
---
+++ sw_site.js
@@ -0,0 +1,52 @@
+//Caching variables
+const cacheName = 'v1';
+
+
+
+// Call install event 
+self.addEventListener('install', e => {
+    console.log('Service Worker: Installed');
+    // adding cache
+});
+
+// Call activate event
+self.addEventListener('activate', e => {
+    console.log('Service Worker: Activated');
+    // Removing unwanted cache
+    e.waitUntil(
+        caches.keys().then( cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if(cache !== cacheName) {
+                        console.log('Service Worker: Clearing Old Cache');
+                        caches.delete(cache);
+                        
+                    }
+                })        
+            )
+        })
+    );
+    
+});
+
+// Call fetch event
+self.addEventListener('fetch', e => {
+    console.log(`Service Worker: Fetching URL: ${e.request.url}`);
+    e.respondWith(
+      fetch(e.request)
+        .then(res => {
+          // Make copy / clone of response
+          console.log('Service Worker: Copying / Clone the response');
+          const resClone = res.clone();
+          // Open cache
+          caches
+            .open(cacheName)
+            .then(cache => {
+              // Add response to cache
+              cache.put(e.request, resClone);
+            });
+          return res;
+      })
+      .catch(err => caches.match(e.request).then(res => res))
+    );
+});
