0cd56632b7a6dacb4a992a99c3287c56c48879c1
Siddhartha Sharma <siddharthasharma79@gmail.com>
2019-07-12 14:43:08 +0530

    sidd-pwa deployment

---
+++ build/js/app.js
@@ -0,0 +1,25 @@
+function createNode(el) {
+    return document.createElement(el);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+console.log('called');
+
+const ul = document.getElementById('people');
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => {
+        let people = data;
+        return people.map(person => {
+            let li = createNode('li');
+            let span = createNode('span');
+            li.innerHTML = person.name;
+            span.innerHTML = person.email;
+            append(li, span);
+            append(ul, li);
+        })
+    
+    });
